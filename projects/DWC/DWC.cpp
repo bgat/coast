e05d2dba7e4b70c86c6240beb80e4a793b4e91cd
Bill Gatliff <gatliffw@amazon.com>
2023-02-28 04:03:21 +0000

    Clean compile of DWC

--- projects/DWC/DWC.cpp
+++ projects/DWC/DWC.cpp
@@ -3,7 +3,6 @@
 #include "../dataflowProtection/dataflowProtection.h"
 
 #include <llvm/Pass.h>
-#include <llvm/PassSupport.h>
 
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/Support/Debug.h>
